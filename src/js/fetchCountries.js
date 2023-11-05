d1e28d50ace3ea09a7c0c6f6721a68ce5ecdfe13
Olya Özel <74108127+curious-minion@users.noreply.github.com>
2023-11-05 22:47:32 +0100

    updated api

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
 // export default fetchCountries(searchQuery), возвращающей промис с массивом стран, результат запроса к API.
 
 export default function fetchCountries(searchQuery) {
-    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
+    const url = `https://restcountries.com/rest/v3.1/name/${searchQuery}`;
 
     return fetch(url)
         .then(response => {return response.json()})
